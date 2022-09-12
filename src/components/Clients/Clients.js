7fea9dd162e0c78bdaefd66863a433cb56903759
abrar997 <abraralrawi997.com>
2022-09-12 18:03:54 +0300

    logo done

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -14,17 +14,17 @@ const Clients = () => {
       </div>
       <div
         className=" grid grid-rows-3 gap-10 xl:gap-5 lg:gap-5 md:gap:7
-         grid-cols-5 xl:grid-cols-5  mt-10
+         grid-cols-2 xl:grid-cols-5  mt-10
          justify-center m-auto 
        
       "
       >
         {data.client.map((item, index) => {
           return (
-            <div key={index} className='flex items-center justify-center'>
+            <div key={index} className="flex items-center justify-center">
               <img
                 src={item.img}
-                className=" w-[200px]  xl:w-[40%] lg:w-[40%] md:w-[40%]"
+                className=" w-[100px]  xl:w-[40%] lg:w-[40%] md:w-[40%]"
               />
             </div>
           );
